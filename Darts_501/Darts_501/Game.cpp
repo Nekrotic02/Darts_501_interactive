c37bd7261613cf72e33e2c1842dde623d97bf60d
Ciaran Byrne <nekrotic@protonmail.com>
2021-04-29 01:32:13 +0100

    Bug Fixes to the Simulation

--- Darts_501/Darts_501/Game.cpp
+++ Darts_501/Darts_501/Game.cpp
@@ -25,59 +25,55 @@ void Game::Play_Game(Results* results, char first)
 	{
 		players[0]->Reset_Score(); // reset the score upon successful complettion of a game
 		players[1]->Reset_Score();
-		while (players[0]->Get_Score() != 0 && players[1]->Get_Score() != 0 && players[0]->Get_Wins() != 7 && players[1]->Get_Wins() != 7) // break if a player has won the significant amount of games over the other
+
+		while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3) // playing for who gets the best of 5 rounds
 		{
-			while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3) // playing for who gets the best of 5 rounds
+			for (short int shots{}; shots < 2; shots++) // for two sets of 3 darts to throw
 			{
-				for (short int shots{}; shots < 2; shots++) // for two sets of 3 darts to throw
+				int temp_score = players[current_player]->Get_Score(); // takes the current player score and stores it into a temp score to work with incase they bust
+				short int hit = NULL; // initialise the hit variable to subtract from the temp score
+				for (short int throws{}; throws < 3; throws++) // throws three darts
 				{
-					int temp_score = players[current_player]->Get_Score(); // takes the current player score and stores it into a temp score to work with incase they bust
-					short int hit = NULL; // initialise the hit variable to subtract from the temp score
-					for (short int throws{}; throws < 3; throws++) // throws three darts
+					new_Throw.Strategy(players[current_player]); // calculate the current player AI strategy to set the single, double or triple target as well as the target value
+					if (players[current_player]->Get_Score() == 50) // if the player score is = 50 throw a bull
 					{
-						new_Throw.Strategy(players[current_player]); // calculate the current player AI strategy to set the single, double or triple target as well as the target value
-
-						if (players[current_player]->Get_Score() == 50) // if the player score is = 50 throw a bull
+						hit = new_Throw.Throw_Bull(players[current_player]);
+					}
+					else
+					{
+						switch (players[current_player]->Get_Strat()) // depending on the multiplier that has been targetted throw for the value targetted
 						{
-							hit = new_Throw.Throw_Bull(players[current_player]);
-						}
-						else
-						{
-							switch (players[current_player]->Get_Strat()) // depending on the multiplier that has been targetted throw for the value targetted
-							{
-							case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
-							case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
-							case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
-							}
-						}
-
-						temp_score = temp_score - hit; // take the hit away from the temp score for checking if it was valid
-						if (temp_score == 1 || temp_score < 0) // bust if the player is in the negative or has a value of 1 and set the score to 50
-						{
-							temp_score = players[current_player]->Get_Score();
-							break;
-						}
-						else if (temp_score == 0) // valid if they won
-						{
-							break;
+						case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
+						case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
+						case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
 						}
 					}
-					players[current_player]->Change_Score(temp_score); // to the current temp score
-					if (players[current_player]->Get_Score() == 0 && players[current_player]->Get_Strat() != 0 && players[current_player]->Get_Strat() != 2) // break the loop if the player has won the game on a double
+					temp_score = temp_score - hit; // take the hit away from the temp score for checking if it was valid
+					if (temp_score == 1 || temp_score < 0) // bust if the player is in the negative or has a value of 1 and set the score to 50
 					{
-						players[current_player]->Increase_Round_Win();
+						temp_score = players[current_player]->Get_Score();
+						break;
+					}
+					else if (temp_score == 0) // valid if they won
+					{
 						break;
 					}
 				}
-				if (players[current_player]->Get_Score() == 0)
+				players[current_player]->Change_Score(temp_score); // to the current temp score
+				if (players[current_player]->Get_Score() == 0) // break the loop if the player has won the game on a double
 				{
-					players[0]->Reset_Score();
-					players[1]->Reset_Score();
+					players[current_player]->Increase_Round_Win();
 					break;
 				}
-				current_player = !current_player; // swap the current player value to access the other pointer in the array
 			}
+			if (players[current_player]->Get_Score() == 0)
+			{
+				players[0]->Reset_Score();
+				players[1]->Reset_Score();
+			}
+			current_player = !current_player; // swap the current player value to access the other pointer in the array
 		}
+
 		if (players[0]->Get_Round_Wins() >= 3) // if player 1 won then add the win
 		{
 			players[0]->Add_Win();
--- Darts_501/Darts_501/Interactive.cpp
+++ Darts_501/Darts_501/Interactive.cpp
@@ -17,142 +17,134 @@ void Interactive::SinglePlayer(Results* results, char first)
 	{
 		players[0]->Reset_Score();
 		players[1]->Reset_Score();
-		while (players[0]->Get_Score() != 0 && players[1]->Get_Score() != 0 && players[0]->Get_Wins() != 7 && players[1]->Get_Wins() != 7)
+		while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3)
 		{
-			while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3)
+			int temp_score = players[current_player]->Get_Score();
+			for (short int shots{}; shots < 2; shots++)
 			{
-				int temp_score = players[current_player]->Get_Score();
-				for (short int shots{}; shots < 2; shots++)
+				for (short int throws{}; throws < 3; throws++)
 				{
-					for (short int throws{}; throws < 3; throws++)
+					int hit{};
+					int strat{};
+					switch (current_player)
 					{
-						int hit{};
-						int strat{};
-						switch (current_player)
+					case 0:
+						new_Throw.Strategy(players[current_player]);
+						std::cout << "AI TURN!!!   SCORE: " << temp_score;
+						if (players[current_player]->Get_Score() == 50)
 						{
-						case 0:
-							new_Throw.Strategy(players[current_player]);
-							std::cout << "AI TURN!!!   SCORE: " << temp_score;
-							if (players[current_player]->Get_Score() == 50)
-							{
-								hit = new_Throw.Throw_Bull(players[current_player]);
-							}
-							else
-							{
-								switch (players[current_player]->Get_Strat())
-								{
-								case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
-								case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
-								case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
-								}
-							}; break;
-
-						case 1:
-							std::cout << players[current_player]->Get_Name() << " YOUR TURN!!\n";
-							std::cout << "SCORE: " << temp_score << "\n\n" << "AI SCORE: " << players[0]->Get_Score() << "\n\n";
-
-							do
-							{
-								std::cout << "Would you like to Throw for \n1) Single \n2) Double \n3) Triple \n4) Bull \nChoice: ";
-								std::cin >> strat;
-								std::cout << std::endl;
-							} while (strat < 1 || strat > 4);
-							std::cout << std::endl;
-							strat--;
-							players[current_player]->Set_Strat(strat);
-
-							int target{};
-
-							if (strat != 3)
-							{
-								do
-								{
-									std::cout << "Please enter your target: ";
-									std::cin >> target;
-									std::cout << std::endl;
-								} while (target < 1 || target > 20);
-
-								players[current_player]->Set_Target(target);
-							}
-							strat--;
-
+							hit = new_Throw.Throw_Bull(players[current_player]);
+						}
+						else
+						{
 							switch (players[current_player]->Get_Strat())
 							{
 							case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
 							case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
 							case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
-							case 3: hit = new_Throw.Throw_Bull(players[current_player]); break;
-							}; break;
-						}
+							}
+						}; break;
 
-						int strat_check = players[current_player]->Get_Strat() + 1;
-						if (hit == (players[current_player]->Get_Target() * strat_check))
+					case 1:
+						std::cout << players[current_player]->Get_Name() << " YOUR TURN!!\n";
+						std::cout << "SCORE: " << temp_score << "\n\n" << "AI SCORE: " << players[0]->Get_Score() << "\n\n";
+						do
 						{
-							std::cout << "\nTarget Hit!!" << std::endl;
-							std::cout << "\nHit: " << hit;
-							std::cout << "\n\n";
-							system("Pause");
-						}
-						else if (strat_check == 4 && hit == 50)
-						{
-							std::cout << "\nBull Hit!!" << std::endl;
-							system("Pause");
-						}
-						else
-						{
-							std::cout << "\nTarget Missed!!";
-							std::cout << "\nHit: " << hit << std::endl;
-							system("Pause");
-						}
-						// score = players[current_player]->Check_Score(hit);
+							std::cout << "Would you like to Throw for \n1) Single \n2) Double \n3) Triple \n4) Bull \nChoice: ";
+							std::cin >> strat;
+							std::cout << std::endl;
+						} while (strat < 1 || strat > 4);
+						std::cout << std::endl;
+						strat--;
+						players[current_player]->Set_Strat(strat);
+						int target{};
 
-						temp_score = temp_score - hit;
-						if (temp_score == 1 || temp_score < 0)
+						if (strat != 3)
 						{
-							std::cout << "\nBust!\n";
-							temp_score = players[current_player]->Get_Score();
-							break;
+							do
+							{
+								std::cout << "Please enter your target: ";
+								std::cin >> target;
+								std::cout << std::endl;
+							} while (target < 1 || target > 20);
+
+							players[current_player]->Set_Target(target);
 						}
-						else if (temp_score == 0 && players[current_player]->Get_Strat() != 0)
+						strat--;
+
+						switch (players[current_player]->Get_Strat())
 						{
-							std::cout << "\nRound Winner!: " << players[current_player]->Get_Name();
-							break;
-						}
+						case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
+						case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
+						case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
+						case 3: hit = new_Throw.Throw_Bull(players[current_player]); break;
+						}; break;
 					}
 
-					players[current_player]->Change_Score(temp_score);
-					if (players[current_player]->Get_Score() == 0)
+					int strat_check = players[current_player]->Get_Strat() + 1;
+					if (hit == (players[current_player]->Get_Target() * strat_check))
 					{
-						players[current_player]->Increase_Round_Win();
+						std::cout << "\nTarget Hit!!" << std::endl;
+						std::cout << "\nHit: " << hit;
+						std::cout << "\n\n";
+						system("Pause");
+					}
+					else if (strat_check == 4 && hit == 50)
+					{
+						std::cout << "\nBull Hit!!" << std::endl;
+						system("Pause");
+					}
+					else
+					{
+						std::cout << "\nTarget Missed!!";
+						std::cout << "\nHit: " << hit << std::endl;
+						system("Pause");
+					}
+					// score = players[current_player]->Check_Score(hit);
+
+					temp_score = temp_score - hit;
+					if (temp_score == 1 || temp_score < 0)
+					{
+						std::cout << "\nBust!\n";
+						temp_score = players[current_player]->Get_Score();
 						break;
 					}
-					system("CLS");
+					else if (temp_score == 0 && players[current_player]->Get_Strat() != 0)
+					{
+						std::cout << "\nRound Winner!: " << players[current_player]->Get_Name();
+						break;
+					}
 				}
+
+				players[current_player]->Change_Score(temp_score);
 				if (players[current_player]->Get_Score() == 0)
 				{
-					players[0]->Reset_Score();
-					players[1]->Reset_Score();
+					players[current_player]->Increase_Round_Win();
 					break;
 				}
-				current_player = !current_player;
+				system("CLS");
 			}
-
-			if (players[0]->Get_Round_Wins() >= 3)
+			if (players[current_player]->Get_Score() == 0)
 			{
-				players[0]->Add_Win();
-
-				std::cout << "Winner!!: " << players[0]->Get_Name() << "\n\n";
+				players[0]->Reset_Score();
+				players[1]->Reset_Score();
 			}
-			else if (players[1]->Get_Round_Wins() >= 3)
-			{
-				players[1]->Add_Win();
-				std::cout << "Winner!!: " << players[1]->Get_Name() << "\n\n";
-			}
-			players[0]->Reset_Round_Wins();
-			players[1]->Reset_Round_Wins();
-			system("Pause");
-			system("CLS");
+			current_player = !current_player;
 		}
+		if (players[0]->Get_Round_Wins() >= 3)
+		{
+			players[0]->Add_Win();
+			std::cout << "Winner!!: " << players[0]->Get_Name() << "\n\n";
+		}
+		else if (players[1]->Get_Round_Wins() >= 3)
+		{
+			players[1]->Add_Win();
+			std::cout << "Winner!!: " << players[1]->Get_Name() << "\n\n";
+		}
+		players[0]->Reset_Round_Wins();
+		players[1]->Reset_Round_Wins();
+		system("Pause");
+		system("CLS");
 	}
 	bool champ;
 	if (players[0]->Get_Wins() > players[1]->Get_Wins())
@@ -176,101 +168,99 @@ void Interactive::MultiPlayer(Results* results, char first)
 	{
 		players[0]->Reset_Score();
 		players[1]->Reset_Score();
-		while (players[0]->Get_Score() != 0 && players[1]->Get_Score() != 0 && players[0]->Get_Wins() != 7 && players[1]->Get_Wins() != 7)
-		{
-			while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3)
-			{
-				for (short int shots{}; shots < 2; shots++)
-				{
-					int temp_score = players[current_player]->Get_Score();
-					for (short int throws{}; throws < 3; throws++)
-					{
-						int hit{};
-						int strat{};
-						std::cout << players[current_player]->Get_Name() << " YOUR TURN!!\n";
-						std::cout << "SCORE: " << temp_score << "\n\n" << players[!current_player]->Get_Name() << " SCORE: " << players[!current_player]->Get_Score() << "\n\n";
 
+		while (players[0]->Get_Round_Wins() != 3 && players[1]->Get_Round_Wins() != 3)
+		{
+			for (short int shots{}; shots < 2; shots++)
+			{
+				int temp_score = players[current_player]->Get_Score();
+				for (short int throws{}; throws < 3; throws++)
+				{
+					int hit{};
+					int strat{};
+					std::cout << players[current_player]->Get_Name() << " YOUR TURN!!\n";
+					std::cout << "SCORE: " << temp_score << "\n\n" << players[!current_player]->Get_Name() << " SCORE: " << players[!current_player]->Get_Score() << "\n\n";
+
+					do
+					{
+						std::cout << "Would you like to Throw for \n1) Single \n2) Double \n3) Triple \n4) Bull \nChoice: ";
+						std::cin >> strat;
+						std::cout << std::endl;
+					} while (strat < 1 || strat > 4);
+					strat--;
+					players[current_player]->Set_Strat(strat);
+
+					int target{};
+
+					if (strat != 3)
+					{
 						do
 						{
-							std::cout << "Would you like to Throw for \n1) Single \n2) Double \n3) Triple \n4) Bull \nChoice: ";
-							std::cin >> strat;
+							std::cout << "Please enter your target: ";
+							std::cin >> target;
 							std::cout << std::endl;
-						} while (strat < 1 || strat > 4);
-						strat--;
-						players[current_player]->Set_Strat(strat);
+						} while (target < 1 || target > 20);
 
-						int target{};
-
-						if (strat != 3)
-						{
-							do
-							{
-								std::cout << "Please enter your target: ";
-								std::cin >> target;
-								std::cout << std::endl;
-							} while (target < 1 || target > 20);
-
-							players[current_player]->Set_Target(target);
-						}
-						strat--;
-
-						switch (players[current_player]->Get_Strat())
-						{
-						case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
-						case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
-						case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
-						case 3: hit = new_Throw.Throw_Bull(players[current_player]); break;
-						}
-						int strat_check = players[current_player]->Get_Strat() + 1;
-						if (hit == (players[current_player]->Get_Target() * strat_check))
-						{
-							std::cout << "\nTarget Hit!!" << std::endl;
-							system("Pause");
-						}
-						else if (players[current_player]->Get_Strat() == 3 && hit == 50)
-						{
-							std::cout << "\nTarget Hit!!" << std::endl;
-							system("Pause");
-						}
-						else
-						{
-							std::cout << "\nTarget Missed!!";
-							std::cout << "\nHit: " << hit << std::endl;
-							system("Pause");
-						}
-						// score = players[current_player]->Check_Score(hit);
-
-						temp_score = temp_score - hit;
-						if (temp_score == 1 || temp_score < 0)
-						{
-							std::cout << "\nBust!\n";
-							temp_score = players[current_player]->Get_Score();
-							break;
-						}
-						else if (temp_score == 0)
-						{
-							break;
-						}
+						players[current_player]->Set_Target(target);
 					}
-					players[current_player]->Change_Score(temp_score);
-					//system("Pause");
-					if (players[current_player]->Get_Score() == 0 && players[current_player]->Get_Strat() != 0)
+					strat--;
+
+					switch (players[current_player]->Get_Strat())
 					{
-						std::cout << "\nRound Winner!: " << players[current_player]->Get_Name();
-						players[current_player]->Increase_Round_Win();
+					case 0: hit = new_Throw.Throw_Single(players[current_player]); break;
+					case 1: hit = new_Throw.Throw_Double(players[current_player]); break;
+					case 2: hit = new_Throw.Throw_Treble(players[current_player]); break;
+					case 3: hit = new_Throw.Throw_Bull(players[current_player]); break;
+					}
+					int strat_check = players[current_player]->Get_Strat() + 1;
+					if (hit == (players[current_player]->Get_Target() * strat_check))
+					{
+						std::cout << "\nTarget Hit!!" << std::endl;
+						system("Pause");
+					}
+					else if (players[current_player]->Get_Strat() == 3 && hit == 50)
+					{
+						std::cout << "\nTarget Hit!!" << std::endl;
+						system("Pause");
+					}
+					else
+					{
+						std::cout << "\nTarget Missed!!";
+						std::cout << "\nHit: " << hit << std::endl;
+						system("Pause");
+					}
+					// score = players[current_player]->Check_Score(hit);
+
+					temp_score = temp_score - hit;
+					if (temp_score == 1 || temp_score < 0)
+					{
+						std::cout << "\nBust!\n";
+						temp_score = players[current_player]->Get_Score();
 						break;
 					}
-					system("CLS");
+					else if (temp_score == 0)
+					{
+						break;
+					}
 				}
-				if (players[current_player]->Get_Score() == 0)
+				players[current_player]->Change_Score(temp_score);
+				//system("Pause");
+				if (players[current_player]->Get_Score() == 0 && players[current_player]->Get_Strat() != 0)
 				{
-					players[0]->Reset_Score();
-					players[1]->Reset_Score();
+					std::cout << "\nRound Winner!: " << players[current_player]->Get_Name();
+					players[current_player]->Increase_Round_Win();
 					break;
 				}
-				current_player = !current_player;
+				system("CLS");
 			}
+			if (players[current_player]->Get_Score() == 0)
+			{
+				players[0]->Reset_Score();
+				players[1]->Reset_Score();
+			}
+			current_player = !current_player;
 		}
+
 		if (players[0]->Get_Round_Wins() >= 3)
 		{
 			players[0]->Add_Win();
